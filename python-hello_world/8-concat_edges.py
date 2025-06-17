e7e8f36677e9baccedb4586b0cb0f88937aca4ca
Nyayath Chol <n.chol@alustudent.com>
2025-06-17 15:23:45 -0700

    8 concat edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,5 +3,4 @@ print("Python is an interpreted, interactive, object-oriented programming"
       " language that combines remarkable power with very clear syntax"[39:67] +
       "Python is an interpreted, interactive, object-oriented programming"
       " language that combines remarkable power with very clear syntax"[107:112] +
-      "Python is an interpreted, interactive, object-oriented programming"
-      " language that combines remarkable power with very clear syntax"[:6])
+      "Python"[0:6])

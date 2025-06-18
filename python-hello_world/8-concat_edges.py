ff3b50a636c6c49185423b0dd5e2b705230bb1c9
Nyayath Chol <n.chol@alustudent.com>
2025-06-18 02:47:24 -0700

    conact edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-holberton = "Holberton School"
-modified_str = holberton[0:9] + holberton[15:] + holberton[9:15]
-print(modified_str)
+print("Holberton School"[0:9] + "Holberton School"[15:] + "Holberton School"[9:15])

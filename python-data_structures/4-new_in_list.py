1bd2ff7b283123952626f23cfb6d1914f2725173
Nyayath Chol <n.chol@alustudent.com>
2025-06-19 14:22:42 -0700

    new in list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-new_list = new_in_list(my_list, 3, 9)
-
-print(new_list)  # [1, 2, 3, 9, 5]
-print(my_list)   # [1, 2, 3, 4, 5] (original unchanged)
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list

1a4cc1bd4d418a486ebf44e70e5f6711b33f509d
Nyayath Chol <n.chol@alustudent.com>
2025-06-19 11:55:40 -0700

    adding python

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,4 +5,21 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    try:
+        print(f"{a} + {b} = {add(a, b)}")
+    except Exception as e:
+        print(f"FAKE add() => {a} - {b}")
+
+    # Additional test cases
+    test_cases = [
+        (10, 30),
+        (-10, 30),
+        (-10, -30),
+        (5, "H"),
+    ]
+
+    for a, b in test_cases:
+        try:
+            print(f"{a} + {b} = {add(a, b)}")
+        except Exception as e:
+            print(f"FAKE add() => {a} - {b}")

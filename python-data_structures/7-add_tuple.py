ea2857401c9ccef20dba8be7af7fc253e25ba724
Nyayath Chol <n.chol@alustudent.com>
2025-06-19 14:41:16 -0700

    add tuple

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+print(add_tuple(tuple_a, tuple_b))     # (89, 100)
+
+print(add_tuple(tuple_a, (1,)))        # (2, 89)
+print(add_tuple(tuple_a, ()))          # (1, 89):

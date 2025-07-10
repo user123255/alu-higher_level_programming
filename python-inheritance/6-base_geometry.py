c77438d461f29024c87e0a0c0f004439758954c9
Nyayath Chol <n.chol@alustudent.com>
2025-07-10 06:12:03 -0700

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for defining the BaseGeometry class."""
+
+class BaseGeometry:
+    """A class representing basic geometry."""
+
+    def area(self):
+        """Calculate the area of the geometry.
+
+        Raises:
+            Exception: area() is not implemented.
+        """
+        raise Exception("area() is not implemented")

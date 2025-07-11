249f849384ef8b07c9642a891e51a1a942a81b59
Nyayath Chol <n.chol@alustudent.com>
2025-07-11 08:58:19 -0700

    task 12

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int and inverts equality operators."""
+"""Defines a class MyInt that inherits from int.
+
+This class inverts the == and != operators.
+"""
 
 
 class MyInt(int):
-    """A rebel integer that inverts == and != operators."""
+    """A rebel integer that inverts == and !=."""
 
     def __eq__(self, other):
         """Invert == to behave like !="""

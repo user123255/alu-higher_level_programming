bf084ad8fbd314258e14826f54124e7246b65551
Nyayath Chol <n.chol@alustudent.com>
2025-07-15 04:53:06 -0700

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Module to read and print text from file"""
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints its content to stdout"""
+    """Reads a UTF-8 text file and prints to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")

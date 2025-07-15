044a20f92831afc219ffaf2e528b766b32d26865
Nyayath Chol <n.chol@alustudent.com>
2025-07-15 09:50:10 -0700

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Module that returns dictionary description for JSON serialization of an object.
+"""
+Function that returns the dictionary description 
+for JSON serialization.
 """
 
-
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object."""
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+    """
     return obj.__dict__

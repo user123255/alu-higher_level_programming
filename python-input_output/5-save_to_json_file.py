3ecb425b2b1da179845a2057c03caa198a62e9d8
Nyayath Chol <n.chol@alustudent.com>
2025-07-15 08:06:33 -0700

    task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that writes an object to a text file using JSON representation."""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using JSON format."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)

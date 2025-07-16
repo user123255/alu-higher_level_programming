687ed38f3074151375e646b65e85b48d5176717e
Nyayath Chol <n.chol@alustudent.com>
2025-07-16 13:00:57 -0700

    error fixed

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-Function that writes a string to a text file (UTF8)
-and returns the number of characters written.
-"""
+"""Module that defines write_file function."""
 
 
 def write_file(filename="", text=""):
-    """Writes `text` to `filename` and returns the number of characters written."""
-    with open(filename, 'w', encoding='utf-8') as f:
+    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)

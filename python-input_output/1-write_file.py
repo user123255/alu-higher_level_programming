4c1421be04e7b71bb061c7fcbc8b0ddec8940548
Nyayath Chol <n.chol@alustudent.com>
2025-07-16 13:08:57 -0700

    error fixed

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,9 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    """
+    Writes a string to a text file (UTF8)
+    and returns the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+        return f.write(tex)

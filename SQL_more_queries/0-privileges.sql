8c06e50e2570d12440e1ca85b473a31d6bdcec0b
Nyayath Chol <n.chol@alustudent.com>
2025-07-21 08:17:47 -0700

    task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,2 @@
--- Ensure user_0d_1 exists and has all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Ensure user_0d_2 exists and has SELECT, INSERT privileges on database user_2_db
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT, INSERT ON `user_2_db`.* TO 'user_0d_2'@'localhost';
-
--- Show privileges for both users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

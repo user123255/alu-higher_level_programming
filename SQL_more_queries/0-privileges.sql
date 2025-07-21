88a9ea412576e3a48f7c47d63c2dac751734302f
Nyayath Chol <n.chol@alustudent.com>
2025-07-21 09:00:42 -0700

    task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';

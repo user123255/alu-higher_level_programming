a0dca3672c308dcfdf15d33295f5cdcca17846de
Nyayath Chol <n.chol@alustudent.com>
2025-07-21 09:02:50 -0700

    task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,9 @@
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
+-- This script creates users and grants specific privileges as required
+
+-- Create user_0d_1 with all privileges (root user)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Create user_0d_2 with SELECT and INSERT privileges on user_2_db
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';

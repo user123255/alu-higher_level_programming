1f8b7ee15c3facb65fd501fd2679237909f34c7b
Nyayath Chol <n.chol@alustudent.com>
2025-07-21 09:45:27 -0700

    task 1

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- 2-create_read_user.sql
+-- Create database hbtn_0d_2 and user user_0d_2 with SELECT privilege only
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;

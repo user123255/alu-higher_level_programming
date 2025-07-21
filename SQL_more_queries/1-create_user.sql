64de1932ca9b7f6d852284d303a9d15d3bfa574c
Nyayath Chol <n.chol@alustudent.com>
2025-07-21 09:39:47 -0700

    task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 1-create_user.sql
+-- Create MySQL user user_0d_1 with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;

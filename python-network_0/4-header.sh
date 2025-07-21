0c68942c1799c42d359591da3042c69c2220aa04
Nyayath Chol <n.chol@alustudent.com>
2025-07-21 08:08:28 -0700

    task 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with a custom header X-HolbertonSchool-User-Id: 98
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"

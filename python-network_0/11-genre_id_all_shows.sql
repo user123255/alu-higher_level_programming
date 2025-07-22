4ebb803c9c51964471f1e98c6aac7fed173c4cd0
Nyayath Chol <n.chol@alustudent.com>
2025-07-22 04:43:26 -0700

    task 11

--- python-network_0/11-genre_id_all_shows.sql
+++ python-network_0/11-genre_id_all_shows.sql
@@ -1,3 +1,6 @@
+-- Lists all shows and their genre IDs (if any) from the hbtn_0d_tvshows database
+-- If a show does not have a genre, it will still appear with NULL
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id

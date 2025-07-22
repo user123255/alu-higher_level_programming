84e4fbab7963851882173f5789e060a6b11e5d3c
Nyayath Chol <n.chol@alustudent.com>
2025-07-22 05:27:39 -0700

    task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 -- main_0_0.sql
--- This script ensures that the users 'user_0d_1' and 'user_0d_2' do not exist on localhost.
--- This is necessary to produce the "There is no such grant defined for user ..." error when checking privileges.
+-- Drop users if they exist to trigger 'no such grant defined' error when queried
 
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 DROP USER IF EXISTS 'user_0d_2'@'localhost';

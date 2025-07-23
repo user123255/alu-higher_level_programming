65dc705b1e00a0df3facd5dd3631658599c88ac6
Nyayath Chol <n.chol@alustudent.com>
2025-07-23 15:10:33 -0700

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""Fetches a URL and displays the response body in a formatted way using urllib"""
+# Fetches https://alu-intranet.hbtn.io/status using urllib and displays formatted response
 
 import urllib.request
 
-# Use this for ALU intranet:
-# url = "https://intranet.hbtn.io/status"
-
-# Use this for local checker testing:
-url = "http://0.0.0.0:5050/status"
+url = "https://alu-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))

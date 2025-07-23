6b84541db8e7efde9a2e0102053a045ff68906f0
Nyayath Chol <n.chol@alustudent.com>
2025-07-23 06:54:13 -0700

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-# 0-hbtn_status.py
-"""Fetches http://0.0.0.0:5050/status using urllib"""
+"""Fetches http://0.0.0.0:5050/status using urllib and prints formatted response"""
 
 import urllib.request
 
-url = "http://0.0.0.0:5050/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+def fetch_status():
+    """Fetch and display HTTP response body in specified format"""
+    url = "http://0.0.0.0:5050/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    fetch_status()

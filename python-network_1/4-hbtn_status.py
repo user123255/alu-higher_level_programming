ba855c3645bf7f683048322a4b3d13b4c472880a
Nyayath Chol <n.chol@alustudent.com>
2025-07-28 03:44:39 -0700

    task 4

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    content = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))

79af5de8570d9540e3c509d103e67fcc9a1e4498
Nyayath Chol <n.chol@alustudent.com>
2025-07-28 03:46:43 -0700

    task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Fetches https://alu-intranet.hbtn.io/status and displays response details."""
+
 import requests
 
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
-
     content = response.text
 
     print("Body response:")

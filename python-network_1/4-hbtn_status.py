4c2d300ada532621dad54027a0f0dabf126db0ca
Nyayath Chol <n.chol@alustudent.com>
2025-07-29 07:45:22 -0700

    task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,11 +8,8 @@ import requests
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    try:
-        response = requests.get(url)
-        content = response.text
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-    except requests.exceptions.RequestException:
-        print("Failed to fetch the status.")
+    response = requests.get(url)
+    content = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))

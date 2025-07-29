2d09bf7799099a4b9b6e051f643ede34a96b6ef5
Nyayath Chol <n.chol@alustudent.com>
2025-07-29 07:19:04 -0700

    task 7

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""
+Sends a request to a given URL and handles errors.
+
+Usage:
+    ./7-error_code.py <URL>
+"""
+
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
 
     if response.status_code >= 400:
-        print(f"Error code: {
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)

644fec40cfb4741e9fe32a85de9828a22dd3b4e0
Nyayath Chol <n.chol@alustudent.com>
2025-07-29 09:31:59 -0700

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status and displays the response body content."""
+"""Fetches https://intranet.hbtn.io/status and displays the response body."""
 
 from urllib import request
-
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"

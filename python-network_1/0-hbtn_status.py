fbaed01c03ae8f6d53380cd012ad30d322ff4af5
Nyayath Chol <n.chol@alustudent.com>
2025-07-29 09:28:26 -0700

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,7 +6,9 @@ from urllib import request
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    with request.urlopen(url) as response:
+    req = request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+
+    with request.urlopen(req) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))

4a768671ad30292f91eda57b1e2156b8da16370e
Nyayath Chol <n.chol@alustudent.com>
2025-07-29 09:18:58 -0700

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using urllib and prints its content."""
+"""Fetches https://intranet.hbtn.io/status and displays the response body content."""
 
 from urllib import request
 

9834b770b542d50ff9a2992bd23e311b0802ee91
Nyayath Chol <n.chol@alustudent.com>
2025-07-29 07:21:33 -0700

    task 8

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to search_user endpoint with letter parameter.
+
+Usage:
+    ./8-json_api.py [letter]
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': q}
+
+    try:
+        response = requests.post(url, data=data)
+        response_json = response.json()
+
+        if response_json:
+            print("[{}] {}".format(response_json.get('id'), response_json.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")

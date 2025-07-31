929a1fecea78ab6031a4b4712e19c13e7333d431
nyayathchol <n.chol@alustudent>
2025-07-31 07:33:06 +0000

    task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Documented now"""
-import requests
+"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+import urllib.request
 
-if __name__ == '__main__':
-    response = requests.get("https://intranet.hbtn.io/status")
+url = "https://alu-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))

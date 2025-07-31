67c26744b962a8aa2651ad568e628fd44b0b70a3
nyayathchol <n.chol@alustudent>
2025-07-31 07:19:10 +0000

    task 4

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status and displays the response body."""
+"""Documented now"""
+import requests
 
-from urllib import request
-
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    headers = {"User-Agent": "Mozilla/5.0"}  # Set a browser-like User-Agent
-    req = request.Request(url, headers=headers)
-
-    with request.urlopen(req) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == '__main__':
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))

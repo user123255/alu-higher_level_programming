f850787bae76cd70cbdfd5848d7d363c1c917105
nyayathchol <n.chol@alustudent>
2025-07-31 07:47:41 +0000

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""Documented now"""
-import requests
+"""I document you"""
+
+
+import urllib.request
+
 
 if __name__ == '__main__':
-    response = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))

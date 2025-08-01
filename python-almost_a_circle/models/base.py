905a6044eaac968668528bef6c3f90e5c7053297
nyayathchol <n.chol@alustudent>
2025-08-01 09:39:19 +0000

    created files and directories

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,21 +1,13 @@
-# tests/test_base.py
+#!/usr/bin/python3
+"""Base class module"""
 
-import unittest
-from models.base import Base
+class Base:
+    """Base class for managing id attribute in all future classes"""
+    __nb_objects = 0
 
-class TestBase(unittest.TestCase):
-    def setUp(self):
-        # Reset counter before each test to keep them independent
-        Base._Base__nb_objects = 0
-
-    def test_id_auto_increment(self):
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b2.id, b1.id + 1)
-
-    def test_id_manual(self):
-        b = Base(99)
-        self.assertEqual(b.id, 99)
-
-if __name__ == '__main__':
-    unittest.main()
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

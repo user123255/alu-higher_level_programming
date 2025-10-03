85cad22459fdec9c015255cdd8cd25fea7a99252
root <n.chol@alustudent.com>
2025-10-03 15:59:07 +0000

    javascript integers

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+if (!num && num !== 0) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}

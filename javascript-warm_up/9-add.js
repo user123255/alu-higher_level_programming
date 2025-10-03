2845592a4626abb74c85512f01571c78a3b6a7bf
root <n.chol@alustudent.com>
2025-10-03 16:48:12 +0000

    javascript add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const num1 = parseInt(args[0]);
+const num2 = parseInt(args[1]);
+
+console.log(add(num1, num2));

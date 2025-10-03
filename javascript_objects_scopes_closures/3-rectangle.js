80709d316f975602fee5ed6a4ba3a277bcc88f2f
root <n.chol@alustudent.com>
2025-10-03 18:04:19 +0000

    Javascript rectangle 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Defines a class Rectangle with width, height, and a print() method
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Print the rectangle using 'X'
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;

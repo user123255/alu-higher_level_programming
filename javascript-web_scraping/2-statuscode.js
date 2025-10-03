b8b1b47434e9569ee980a8cdd5e64cce6043bb1a
root <n.chol@alustudent.com>
2025-10-03 19:04:20 +0000

    statuscode

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that displays the status code of a GET request
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (!error) {
+    console.log(`code: ${response.statusCode}`);
+  }
+});

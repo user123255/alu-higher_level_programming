359335ac4fdb737572e4a2c8bb6551112af4c7ca
root <n.chol@alustudent.com>
2025-10-03 18:22:55 +0000

    Javascript 8 esrever

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Function that returns the reversed version of a list without using Array.reverse
+exports.esrever = function (list) {
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};

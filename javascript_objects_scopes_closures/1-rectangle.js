d7d9714aa6384448e50ed6ee805a02daa42ca95b
root <n.chol@alustudent.com>
2025-10-03 17:54:30 +0000

    javascript rectangle 1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Defines a class Rectangle with width and height
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;

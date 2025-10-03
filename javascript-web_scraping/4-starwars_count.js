51a07ffda93afd09330d1d7b5e8e8bc7a6332377
root <n.chol@alustudent.com>
2025-10-03 19:34:44 +0000

    starwars js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
-const fs = require('fs');
 const request = require('request');
 
-const pathOrUrl = process.argv[2];
+const url = process.argv[2];
 
-if (!pathOrUrl) {
+if (!url) {
   console.log(0);
-} else if (pathOrUrl.startsWith('http://') || pathOrUrl.startsWith('https://')) {
-  // Use request for URLs
-  request(pathOrUrl, { json: true }, (err, response, body) => {
-    if (err) return console.log(0);
-    // Assuming the body is an array or object with array
-    console.log(body.length || 0);
-  });
 } else {
-  // Use fs for local files
-  fs.readFile(pathOrUrl, 'utf-8', (err, data) => {
+  request(url, { json: true }, (err, response, body) => {
     if (err) return console.log(0);
-    const lines = data.split('\n').filter(Boolean);
-    console.log(lines.length);
+
+    // SWAPI returns an object with a 'results' array
+    if (!body || !body.results) return console.log(0);
+
+    let count = 0;
+    body.results.forEach(film => {
+      if (film.characters && film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count += 1;
+      }
+    });
+    console.log(count);
   });
 }
